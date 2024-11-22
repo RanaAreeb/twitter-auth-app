17132e3fbcc4b4b07163925500585b82ec94a698
Rana Muhammad Areeb <115886881+RanaAreeb@users.noreply.github.com>
2024-11-22 14:28:02 +0500

    added

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -19,6 +19,7 @@ export default async function handler(req, res) {
   // Construct the Twitter OAuth URL
   const twitterAuthUrl = `${process.env.NEXT_PUBLIC_TWITTER_AUTH_URL}?response_type=code&client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.NEXT_PUBLIC_REDIRECT_URI)}&scope=tweet.read users.read like.read like.write&state=state123&code_challenge=${codeChallenge}&code_challenge_method=S256`;
 
+
   // Redirect the user to the Twitter OAuth page
   res.redirect(twitterAuthUrl);
 }
